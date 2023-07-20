a9d793c3313cf7190f557ed55554776f156bbc04
SeongSikChae <55735227+SeongSikChae@users.noreply.github.com>
2023-07-20 16:41:02 +0900

    Asynchronous Locking Addition

---
+++ PowerManagerLib/System/Threading/Tasks/AsyncLock.cs
@@ -0,0 +1,43 @@
+﻿namespace System.Threading.Tasks
+{
+    public class AsyncLock : IDisposable
+    {
+        private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+
+        public async Task LockAsync(Func<Task> worker)
+        {
+            try
+            {
+                await semaphoreSlim.WaitAsync();
+                await worker();
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                    semaphoreSlim.Dispose();
+                disposedValue = true;
+            }
+        }
+
+        // ~AsyncLock()
+        // {
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
