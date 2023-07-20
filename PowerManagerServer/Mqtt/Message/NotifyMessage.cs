d98209d7f0d042e058b7a9163db0c04632fdce17
SeongSikChae <55735227+SeongSikChae@users.noreply.github.com>
2023-07-20 16:45:33 +0900

    MQTT Pub/Sub Message 추가

---
+++ PowerManagerServer/Mqtt/Message/NotifyMessage.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PowerManagerServer.Mqtt.Message
+{
+    public sealed class NotifyMessage
+    {
+        [JsonPropertyName("sid")]
+        public string Sid { get; set; } = "2";
+
+        [JsonPropertyName("msg")]
+        public MsgPayload? Msg { get; set; }
+
+        public sealed class MsgPayload
+        {
+            [JsonPropertyName("o")]
+            public string O { get; set; } = "n";
+
+            [JsonPropertyName("e")]
+            public List<Body> E { get; set; } = new List<Body>();
+
+            public sealed class Body
+            {
+                [JsonPropertyName("n")]
+                public string? N { get; set; }
+
+                [JsonPropertyName("sv")]
+                public string? Sv { get; set; }
+
+                [JsonPropertyName("ti")]
+                public string? Time { get; set; }
+            }
+        }
+    }
+}
---
+++ PowerManagerServer/Mqtt/Message/ReadMessage.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PowerManagerServer.Mqtt.Message
+{
+    public sealed class ReadMessage
+    {
+        [JsonPropertyName("sid")]
+        public string Sid { get; set; } = "2";
+
+        [JsonPropertyName("device_id")]
+        public string DeviceId { get; set; } = string.Empty;
+
+        [JsonPropertyName("msg")]
+        public MsgPayload? Msg { get; set; }
+
+        public sealed class MsgPayload
+        {
+            [JsonPropertyName("o")]
+            public string O { get; set; } = "r";
+
+            [JsonPropertyName("e")]
+            public List<Body> E { get; set; } = new List<Body>();
+
+            public sealed class Body
+            {
+                [JsonPropertyName("n")]
+                public string N { get; set; } = string.Empty;
+
+                [JsonPropertyName("ti")]
+                public string Time { get; set; } = string.Empty; 
+            }
+        }
+    }
+}
